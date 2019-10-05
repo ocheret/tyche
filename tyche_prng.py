8687e1b80e7e82061a8f0d1ae1ff8141853c7eab
Chuck Ocheret <ocheret@panix.com>
2019-10-05 16:51:40 -0400

    Added first cut at entropy pools.

--- tyche_prng.py
+++ tyche_prng.py
@@ -9,7 +9,7 @@ def increment_counter(counter: bytearray) -> None:
     # If the counter comes back around to 0 again then we've survived the heat death of the Universe
     for i in range(len(counter)):
         if counter[i] != 255:
-            counter[i] = counter[i] + 1
+            counter[i] += 1
             return
         counter[i] = 0
 
@@ -17,6 +17,14 @@ def increment_counter(counter: bytearray) -> None:
 def sha256_hash(to_be_hashed: bytearray) -> bytearray:
     """Convenience wrapper for a SHA256 hash that returns a bytearray."""
     return bytearray(hashlib.sha256(to_be_hashed).digest())
+
+
+def sha256_hash_all(*args):
+    """Convenience wrapper for SHA256 hashing of multiple chunks in separate bytearrays"""
+    h = hashlib.sha256()
+    for a in args:
+        h.update(a)
+    return bytearray(h.digest())
 
 
 class SimpleAES(object):
@@ -43,6 +51,57 @@ class SimpleAES(object):
         return self.encryptor.update(block)
 
 
+class EntropyPools(object):
+    """
+    Maintain pools of entropy a la Fortuna.
+
+    Entropy is added to different pools in a round robin fashion. When entropy is used to reseed a PRNG,
+    a different number of pools is used to make attacks more difficult.
+    """
+    POOL_COUNT = 32
+    POOL_BITS = 256
+    POOL_BYTES = POOL_BITS // 8
+
+    def __init__(self):
+        # As in Fortunat, we maintain 32 pools of entropy.
+        # Each pool is 256 bits long and keep them all in a single bytearray.
+        self.pools = bytearray(self.POOL_COUNT * self.POOL_BYTES)
+
+        # As new entropy arrives, we add it to each pool in sequence
+        self.next_pool = 0
+
+    def add_entropy(self, new_entropy: bytearray):
+        """Add new entropy. It will be added to the next pool in the sequence."""
+        # Determine where the current pool is in the bytearray and extract it
+        start = self.next_pool * self.POOL_BYTES
+        end = start + self.POOL_BYTES
+        old_entropy = self.pools[start:end]
+
+        # Mix the new entropy with the old entropy pool
+        entropy = sha256_hash_all(old_entropy, new_entropy)
+
+        # Reinsert the entropy back into the pool
+        self.pools[start:end] = entropy
+
+        # Advance to the next pool
+        self.next_pool = (self.next_pool + 1) & (self.POOL_COUNT - 1)
+
+    def get_entropy(self, iteration) -> bytearray:
+        """
+        Retrieve entropy from the pools. The iteration determines which subset of pools are used.
+
+        As with Fortuna, we use entroy from the first N pools based on the iteration count.
+        """
+        # The number of pools to use is determined by the least significant but of the current iteration.
+        # Pool 0 is used for ever reseed, Pool 1 for ever other reseed, Pool 2 for every 4th reseed, etc..
+        bits = iteration | (2 << self.POOL_COUNT)
+        number_of_pools = ((1 + (bits ^ (bits - 1))) >> 1) + 1
+
+        # Retrieve the correct number of pools from the front of the pools array.
+        end = number_of_pools * self.POOL_BYTES
+        return self.pools[:end]
+
+
 class TychePRNG(object):
     """
     A implementation of a Fortuna-like PRNG simplified for this use case.
@@ -67,6 +126,9 @@ class TychePRNG(object):
         # Setup the cryptopgraphy package for AES in CTR mode
         self.aes = SimpleAES()
 
+        # Maintain pools of entropy
+        self.entropy_pools = EntropyPools()
+
     def reseed(self, additional_seed: bytearray):
         """Seed with additional entropy"""
 
@@ -84,7 +146,7 @@ class TychePRNG(object):
         # We won't check num_blocks since we will call it with a reasonable value
         blocks = bytearray()
         for i in range(num_blocks):
-            blocks = blocks + self.aes.encrypt_block(self.counter)
+            blocks += self.aes.encrypt_block(self.counter)
             increment_counter(self.counter)
 
         return blocks
